c74c2c299207ac5e6070f3017b4c0b535077b08a
Pusic Filip <la216187@student.helha.be>
2024-11-25 16:15:45 +0100

    Authenticate fixed

--- src/main/java/helha/be/mongojdbc/configuration/UserDetailsServiceConfig.java
+++ src/main/java/helha/be/mongojdbc/configuration/UserDetailsServiceConfig.java
@@ -1,15 +1,26 @@
 package helha.be.mongojdbc.configuration;
 
+import helha.be.mongojdbc.models.Users;
+import helha.be.mongojdbc.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 public class UserDetailsServiceConfig implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepo;
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -28,6 +39,18 @@ public class UserDetailsServiceConfig implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Users user = userRepo.findByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException(username);
+        }
+        UserDetails details = new User(user.getUsername(), user.getPassword(), getAuthorities(user.getRole()));
+
+        return details;
+    }
+
+    private List<GrantedAuthority> getAuthorities(String role){
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+role.toUpperCase()));
+        return authorities;
     }
 }
