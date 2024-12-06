48ad5e2326a894099e9f674f14da9d367406a96a
la226813 <la226813@student.helha.be>
2024-12-06 12:31:37 +0100

    test backend

--- Backend/src/main/java/helha/be/mongodb/Model/LoginRequest.java
+++ Backend/src/main/java/helha/be/mongodb/Model/LoginRequest.java
@@ -1,33 +1,16 @@
 package helha.be.mongodb.Model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
 
     private String email;
     private String password;
     private String token;
-
-    // Getters and Setters
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getToken(){
-        return token;
-    }
-
-    public void setToken(String token){
-        this.token = token;
-    }
 }
--- Backend/src/test/java/helha/be/mongodb/MongoJdbcApplicationTests.java
+++ Backend/src/test/java/helha/be/mongodb/MongoJdbcApplicationTests.java
@@ -12,4 +12,57 @@ class MongoJdbcApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void testCreateForm() {
+    }
+
+    @Test
+    void testCreateReponse() {
+    }
+
+    @Test
+    void testCreateUser() {
+    }
+
+    @Test
+    void testGetAllForms() {
+    }
+
+    @Test
+    void testGetFormById() {
+    }
+
+    @Test
+    void testGetAllReponses() {
+    }
+
+    @Test
+    void testGetReponseById() {
+    }
+
+    @Test
+    void testGetAllUsers() {
+    }
+
+    @Test
+    void testGetUserById() {
+    }
+
+    @Test
+    void testGetUserByEmail() {
+    }
+
+    @Test
+    void testLogin() {
+    }
+    @Test
+    void testDeleteForm() {
+    }
+    @Test
+    void testDeleteReponse() {
+    }
+    @Test
+    void testDeleteUser() {
+    }
+
 }
