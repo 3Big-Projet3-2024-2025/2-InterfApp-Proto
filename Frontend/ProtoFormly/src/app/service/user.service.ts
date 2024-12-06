69e54f86ab44e6a0f833ed5b6dba012264d3bba4
la226813 <la226813@student.helha.be>
2024-12-06 01:22:32 +0100

    decode jwt

--- Frontend/ProtoFormly/src/app/service/user.service.ts
+++ Frontend/ProtoFormly/src/app/service/user.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { jwtDecode, JwtPayload } from 'jwt-decode';
+import { error } from 'console';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +10,19 @@ import { Observable } from 'rxjs';
 export class UserService {
 
   private apiUrl = 'http://localhost:8080/api/users'; 
+  private tokenJWT : any = "";
 
   constructor(private http: HttpClient) { }
 
   isAuthenticated() :boolean{
-    return true;
+    return this.tokenJWT
   }
 
-  hasRole(role : any) : boolean{
-    return true;
+  hasRole( role: string): boolean {
+    if (!this.isAuthenticated){
+      return false;
+    }
+    return this.tokenJWT.roles.includes(role);
   }
 
   register(userData: any): Observable<any> {
@@ -25,8 +31,21 @@ export class UserService {
   }
 
   login(userData: any): Observable<any> {
-   
+
     return this.http.post(this.apiUrl+"/login", userData);
+  }
+
+  saveJwt(jwt: string){
+    this.tokenJWT = jwtDecode(jwt);
+  }
+
+
+  isTokenExpired(token: string): boolean {
+    if (!this.tokenJWT || !this.tokenJWT.exp) {
+      return true; // Si le token ne contient pas d'expiration, on considère qu'il est expiré
+    }
+    const currentTime = Math.floor(Date.now() / 1000); // Temps actuel en secondes
+    return this.tokenJWT.exp < currentTime;
   }
 }
 
